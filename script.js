6d938c18696e7984e84b062797ce5c0f4d4754b4
Jasurbek Aripov <68148826+aripovjr@users.noreply.github.com>
2021-11-18 20:35:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,6 @@
 const showBtn = document.getElementById('show')
 const hideBtn = document.getElementById('hide')
 const links = document.querySelector('.link-container')
-
 showBtn.addEventListener('click', ()=>{
     links.classList.add('show')
 })
@@ -18,9 +17,7 @@ const speedRange = 8
 const text = "Frontend Developer / Student at University of Lodz"
 let idx = 1
 let speed = 1000/speedRange
-
 writeText()
-
 function writeText(){
     textEl.innerHTML = text.slice(0, idx)
     idx++
@@ -30,3 +27,23 @@ function writeText(){
     setTimeout(writeText, speed)
 }
 //finish the code above
+
+//Generate API of google for map
+initMap()
+
+function initMap(){
+    var location = {lat: 51.776016, lng: 19.485582}
+    var current_location = {lat: 40.974026, lng: 29.151094}
+    var map = new google.maps.Map(document.getElementById('map'),{
+        zoom: 4,
+        center: location
+    })
+    var marker = new google.maps.Marker({
+        position: location,
+        map: map
+    })
+    var current_mark = new google.maps.Marker({
+        position: current_location,
+        map: map
+    }) 
+}
