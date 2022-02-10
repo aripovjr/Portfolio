fb1f6b88592554660d83f112efa417e2153c2888
Jasurbek Aripov <jasurbek_aripov@mail.ru>
2022-02-10 03:53:49 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,66 +1,81 @@
 const projects = [
     {
-    img: './pagination.png',
+    img: './images/pagination.png',
     title: 'pagenation',
     link: 'https://github.com/aripovjr/pagination-GitHub',
     website: 'https://xenodochial-turing-0f287b.netlify.app/'
     },
     {
-    img: './es-6slider.png',
+    img: './images/es-6slider.png',
     title: 'ES6-Slider',
     link: 'https://github.com/aripovjr/ES6-Silder',
     website: 'https://tender-banach-b72cb6.netlify.app/'
     },
     {
-    img: 'pagination.png',
-    title: 'pagenation',
-    link: 'http://github.com'
+    img: './images/.png',
+    title: 'to do list',
+    link: 'https://github.com/aripovjr/To-Do-List',
+    website: 'https://pensive-ptolemy-887b8d.netlify.app/'
     },
     {
-    img: 'pagination.png',
-    title: 'pagenation',
-    link: 'http://github.com'
-    },
-    {
-    img: 'pagination.png',
-    title: 'pagenation',
-    link: 'http://github.com'
-    },
-    {
-    img: 'pagination.png',
-    title: 'pagenation',
-    link: 'http://github.com'
-    },
-    {
-    img: 'pagination.png',
-    title: 'pagenation',
-    link: 'http://github.com'
-    },
-    {
-    img: 'pagination.png',
-    title: 'pagenation',
-    link: 'http://github.com'
-    },
-    {
-    img: 'pagination.png',
-    title: 'pagenation',
-    link: 'http://github.com',
+    img: './images/.png',
+    title: 'reviews',
+    link: 'https://github.com/aripovjr/Reviews',
     website: 'https://blissful-lewin-469690.netlify.app/'
     },
     {
-    img: 'pagination.png',
-    title: 'pagenation',
-    link: 'http://github.com'
+    img: './images/pagination.png',
+    title: 'live user list',
+    link: 'https://github.com/aripovjr/Live-User-List',
+    website: 'https://musing-ramanujan-3ef798.netlify.app/'
     },
     {
-    img: 'pagination.png',
-    title: 'pagenation',
-    link: 'http://github.com'
+    img: './images/pagination.png',
+    title: 'movie app',
+    link: 'https://github.com/aripovjr/Movie-App',
+    website: 'https://happy-clarke-10bff0.netlify.app/'
     },
     {
-    img: 'pagination.png',
-    title: 'pagenation',
-    link: 'http://github.com'
-    }
+    img: './images/pagination.png',
+    title: 'catch game',
+    link: 'https://github.com/aripovjr/Insect-Catch-Gamet',
+    website: 'https://serene-kowalevski-c99a4f.netlify.app/'
+    },
+    {
+    img: './images/pagination.png',
+    title: 'note app',
+    link: 'https://github.com/aripovjr/Note-App',
+    website: 'https://note-ap-p.netlify.app/'
+    },
+    {
+    img: './images/pagination.png',
+    title: 'paint app',
+    link: 'https://github.com/aripovjr/Drowing-App',
+    website: 'https://wizardly-wright-469a3d.netlify.app/'
+    },
+    {
+    img: './images/pagination.png',
+    title: 'quiz app',
+    link: 'https://github.com/aripovjr/Quiz-App',
+    website: 'https://inspiring-jackson-38f9a1.netlify.app/'
+    },
+    {
+    img: './images/pagination.png',
+    title: 'clock',
+    link: 'https://github.com/aripovjr/clock',
+    website: 'https://awesome-hawking-ba6fc0.netlify.app/'
+    },
+    {
+    img: './images/pagination.png',
+    title: 'counter',
+    link: 'https://github.com/aripovjr/counter',
+    website: 'https://nervous-payne-3cea59.netlify.app/'
+    },
+    {
+    img: './images/pagination.png',
+    title: 'testimonial',
+    link: 'https://github.com/aripovjr/Testimonial-Box-Switcher',
+    website: 'https://dazzling-roentgen-6c87b6.netlify.app/'
+    },
 ]
 export default projects
