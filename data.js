337ff79eadb2e76f3d39a91a2dc29b5d51ab71cd
Jasurbek Aripov <68148826+aripovjr@users.noreply.github.com>
2022-02-11 00:52:08 +0300

    Update data.js

--- data.js
+++ data.js
@@ -12,88 +12,112 @@ const projects = [
     website: 'https://tender-banach-b72cb6.netlify.app/'
     },
     {
-    img: './images/.png',
+    img: './images/to-do.png',
     title: 'to do list',
     link: 'https://github.com/aripovjr/To-Do-List',
     website: 'https://pensive-ptolemy-887b8d.netlify.app/'
     },
     {
-    img: './images/.png',
-    title: 'reviews',
-    link: 'https://github.com/aripovjr/Reviews',
-    website: 'https://blissful-lewin-469690.netlify.app/'
+    img: './images/github-search.png',
+    title: 'Search user',
+    link: 'https://github.com/aripovjr/Github-Profile-Searcher',
+    website: 'https://ecstatic-hypatia-628dc6.netlify.app/'
     },
     {
-    img: './images/pagination.png',
+    img: './images/live-uzer.png',
     title: 'live user list',
     link: 'https://github.com/aripovjr/Live-User-List',
     website: 'https://musing-ramanujan-3ef798.netlify.app/'
     },
     {
-    img: './images/pagination.png',
+    img: './images/movie.png',
     title: 'movie app',
     link: 'https://github.com/aripovjr/Movie-App',
     website: 'https://happy-clarke-10bff0.netlify.app/'
     },
     {
-    img: './images/pagination.png',
+    img: './images/insect.png',
     title: 'catch game',
     link: 'https://github.com/aripovjr/Insect-Catch-Gamet',
     website: 'https://serene-kowalevski-c99a4f.netlify.app/'
     },
     {
-    img: './images/pagination.png',
+    img: './images/note-app.png',
     title: 'note app',
     link: 'https://github.com/aripovjr/Note-App',
     website: 'https://note-ap-p.netlify.app/'
     },
     {
-    img: './images/pagination.png',
+    img: './images/paint.png',
     title: 'paint app',
     link: 'https://github.com/aripovjr/Drowing-App',
     website: 'https://wizardly-wright-469a3d.netlify.app/'
     },
     {
-    img: './images/pagination.png',
+    img: './images/quiz.png',
     title: 'quiz app',
     link: 'https://github.com/aripovjr/Quiz-App',
     website: 'https://inspiring-jackson-38f9a1.netlify.app/'
     },
     {
-    img: './images/pagination.png',
+    img: './images/clock.png',
     title: 'clock',
     link: 'https://github.com/aripovjr/clock',
     website: 'https://awesome-hawking-ba6fc0.netlify.app/'
     },
     {
-    img: './images/pagination.png',
+    img: './images/counter.png',
     title: 'counter',
     link: 'https://github.com/aripovjr/counter',
     website: 'https://nervous-payne-3cea59.netlify.app/'
     },
     {
-    img: './images/pagination.png',
+    img: './images/testimonial.png',
     title: 'testimonial',
     link: 'https://github.com/aripovjr/Testimonial-Box-Switcher',
     website: 'https://dazzling-roentgen-6c87b6.netlify.app/'
     },
     {
-    img: './images/pagination.png',
-    title: 'testimonial',
-    link: 'https://github.com/aripovjr/Testimonial-Box-Switcher',
-    website: 'https://dazzling-roentgen-6c87b6.netlify.app/'
+    img: './images/hoverboard.png',
+    title: 'hoverboard',
+    link: 'https://github.com/aripovjr/Hoverboard',
+    website: 'https://eloquent-hopper-5248ed.netlify.app/'
     },
     {
-    img: './images/pagination.png',
-    title: 'testimonial',
-    link: 'https://github.com/aripovjr/Testimonial-Box-Switcher',
-    website: 'https://dazzling-roentgen-6c87b6.netlify.app/'
+    img: './images/password-generator.png',
+    title: 'generator',
+    link: 'https://github.com/aripovjr/Password-Generator',
+    website: 'https://loving-snyder-926561.netlify.app/'
     },
     {
-    img: './images/pagination.png',
-    title: 'testimonial',
-    link: 'https://github.com/aripovjr/Testimonial-Box-Switcher',
-    website: 'https://dazzling-roentgen-6c87b6.netlify.app/'
+    img: './images/blur-form.png',
+    title: 'blur form',
+    link: 'https://github.com/aripovjr/Password-Strength-Background',
+    website: 'https://xenodochial-easley-20aca1.netlify.app/'
+    },
+    {
+    img: './images/review.png',
+    title: 'reviews',
+    link: 'https://github.com/aripovjr/Reviews',
+    website: 'https://blissful-lewin-469690.netlify.app/'
+    },
+    {
+    img: './images/faq.png',
+    title: 'FAQ',
+    link: 'https://github.com/aripovjr/FAQ',
+    website: 'https://agitated-snyder-0bcaa8.netlify.app/'
+    },
+    {
+    img: './images/wave-form.png',
+    title: 'wave form',
+    link: 'https://github.com/aripovjr/Waving-Form',
+    website: 'https://unruffled-chandrasekhar-528927.netlify.app/'
+    },
+    {
+    img: './images/toggle-menupng.png',
+    title: 'Animated Nav',
+    link: 'https://github.com/aripovjr/Animated-Navigation',
+    website: 'https://nifty-saha-5dfaba.netlify.app/'
     },
 ]
 export default projects
