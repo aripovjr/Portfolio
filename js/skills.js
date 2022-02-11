844d9bf449ffe6feb62251b8c29b86bbba4d92d2
Jasurbek Aripov <jasurbek_aripov@mail.ru>
2022-02-11 08:08:38 +0300

    Create skills.js

---
+++ js/skills.js
@@ -0,0 +1,34 @@
+const skillsData = [
+    {
+        log: '<i class="fab fa-html5 </i>"',
+        title: 'HTML',
+        text: '',
+    },
+    {
+        log: 'fab fa-css3-alt',
+        title: 'CSS',
+        text: ''
+    },
+    {
+        log: 'fab fa-js',
+        title: 'JavaScript',
+        text: ''
+    },
+    {
+        log: 'fab fa-bootstrap',
+        title: 'Bootstrap',
+        text: ''
+    },
+    {
+        log: 'fab fa-react',
+        title: 'React',
+        text: ''
+    },
+    {
+        log: 'fab fa-git-alt',
+        title: 'Git',
+        text: ''
+    },
+]
+
+export default skillsData
